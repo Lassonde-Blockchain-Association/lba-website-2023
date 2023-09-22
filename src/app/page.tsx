b94f3312771d118524d88522d4f3a6ae33eb8e08
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2023-09-22 01:47:55 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
+import React from 'react';
+import Mission from './components/mission';
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="LBA">
+      <div >
+        <Mission />
+      </div>
     </main>
-  )
+  );
 }
