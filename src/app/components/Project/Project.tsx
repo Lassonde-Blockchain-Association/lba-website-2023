a2d80fb0f746ddbf581dac2d5bc8dbae7d8f1f63
DWKim67 <dkim67@my.yorku.ca>
2023-09-23 23:02:33 -0400

    Merged code with Daniel + Samson, adjusted errors

--- src/app/components/Project/Project.tsx
+++ src/app/components/Project/Project.tsx
@@ -1,76 +1,23 @@
 "use client";
-import React, { MutableRefObject, useEffect, useRef } from "react";
-import Image from "next/image";
-import "./timeline.css";
+import React, { useEffect, useRef } from "react";
 import Timeline from "../Timeline";
 import data from "./TimelineData";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-   
-
-interface TimelineItemProps {
-  date: string;
-  title: string;
-  description: string;
-}
-
-const timelineData: TimelineItemProps[] = [
-  {
-    date: "Jan 2023",
-    title: "Project Kickoff",
-    description: "Met with the team and defined the scope of the project.",
-  },
-  {
-    date: "Feb 2023",
-    title: "Design Phase",
-    description: "Developed wireframes and high-fidelity prototypes.",
-  },
-];
-
-// const TimelineItem: React.FC<TimelineItemProps> = ({
-//   date,
-//   title,
-//   description,
-// }) => (
-//   <div className="flex items-center relative mb-16 last:mb-0">
-//     <div className="z-10 w-16 h-16 relative">
-//       <img src="/isometric_cube.png"></img>
-//     </div>
-//     {/* <div className="ml-8 bg-white rounded-lg p-4 shadow-lg border border-gray-200 w-72">
-//       <div className="flex items-center justify-between mb-2"> */}
-//     <li className="direction-r">
-//       <div className="relative">
-//         <div className="flex justify-end items-center space-x">
-//           <span className="flag bg-gray-100 px-2.5 py-1.5 rounded-md shadow-md relative text-left font-semibold">
-//             {title}
-//           </span>
-
-//           <span className="bg-gray-50 text-red-400 text-sm px-2">{date}</span>
-//         </div>
-//         <p className="desc mt-3 italic text-sm leading-relaxed">
-//           {description}
-//         </p>
-//       </div>
-//       {/* Timeline line */}
-//       <div className="absolute top-[80%] left-[30px] z-0 w-1 h-[115%] bg-gray-400"></div>
-//     </li>
-//   </div>
-// );
 
 const Project: React.FC = () => {
   console.log("ScrollSection component rendered"); // Add this line
 
-
-  
+  const timelineRef = useRef<HTMLDivElement>(null);
 
   const sectionRef = useRef<HTMLDivElement | null>(null);
   const triggerRef = useRef<HTMLDivElement | null>(null);
 
   gsap.registerPlugin(ScrollTrigger);
   console.log(sectionRef); // Add this line
-      
+
   const bodyRef = useRef(null);
-  const maskRef = useRef(null);
+  const maskRef = useRef(null); // Needed for the max
   const projectBodyColour = "gray-200";
 
   const bodyClassName: string = `bg-${projectBodyColour} min-h-screen p-8`;
@@ -78,46 +25,44 @@ const Project: React.FC = () => {
   const maskClassName: string = `absolute bg-${projectBodyColour} h-16 w-3/4`;
 
   useEffect(() => {
-     const handleScroll = () => {
-       if (timelineRef.current) {
-         const timelineBlocks = Array.from(timelineRef.current.children);
-         timelineBlocks.forEach((block) => {
-           if (
-             block.getBoundingClientRect().top <= window.innerHeight * 0.75 &&
-             block.classList.contains("is-hidden")
-           ) {
-             block.classList.remove("is-hidden");
-             block.classList.add("bounce-in");
-           }
-         });
-       }
-     };
+    const handleScroll = () => {
+      if (timelineRef.current) {
+        const timelineBlocks = Array.from(timelineRef.current.children);
+        timelineBlocks.forEach((block) => {
+          if (
+            block.getBoundingClientRect().top <= window.innerHeight * 0.75 &&
+            block.classList.contains("is-hidden")
+          ) {
+            block.classList.remove("is-hidden");
+            block.classList.add("bounce-in");
+          }
+        });
+      }
+    };
 
     gsap.to(maskRef.current, {
       width: "5%",
       x: 800,
       duration: 2,
-      /*
-      ease: "none",
-      delay: 1,*/
       // Trying to figure out how to sync it with the scroll
       scrollTrigger: {
         trigger: bodyRef.current,
         markers: true,
         start: "top left",
         scrub: 1,
+        pin: true,
       },
     });
 
     handleScroll();
 
-
+    /*
     console.log("Running useEffect");
 
     if (!sectionRef.current || !triggerRef.current) {
       console.log("Refs not attached");
       return;
-    }
+    }*/
 
     const pin = gsap.fromTo(
       sectionRef.current,
@@ -139,63 +84,27 @@ const Project: React.FC = () => {
       }
     );
     window.addEventListener("scroll", handleScroll);
-
-//     return () => {
-//       window.removeEventListener("scroll", handleScroll);
-//     };
   }, []);
 
-  {
-    /* 
-    <div className="bg-gray-200 min-h-screen p-8">
-      <h2 className="text-3xl font-bold mb-8">Project Timeline</h2>
-      <div ref={timelineRef} className="relative">
-        {timelineData.map((item, idx) => (
-          <TimelineItem key={idx} {...item} />
-        ))}
-      </div>
-    </div>*/
-  }
   return (
     <div ref={bodyRef} className={bodyClassName}>
       <h2 className="text-3xl font-bold mb-8">Project Timeline</h2>
       <div>
         <div className="absolute">
-          <Timeline className="flex flex-start h-16 opacity-100 " length={5} />
+          <Timeline
+            className="flex flex-start h-16 opacity-100 "
+            length={data.length}
+          />
         </div>
         <div ref={maskRef} className={maskClassName}></div>
         <div className="absolute">
-          <Timeline className="flex flex-start h-16 opacity-40" length={5} />
+          <Timeline
+            className="flex flex-start h-16 opacity-40"
+            length={data.length}
+          />
         </div>
       </div>
       <section className="">
-        <svg
-          viewBox="0 0 900 10"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M9.89998 6C9.43671 8.28224 7.41896 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.41896 0 9.43671 1.71776 9.89998 4H445.1C445.563 1.71776 447.581 0 450 0C452.419 0 454.437 1.71776 454.9 4H890.1C890.563 1.71776 892.581 0 895 0C897.761 0 900 2.23858 900 5C900 7.76142 897.761 10 895 10C892.581 10 890.563 8.28224 890.1 6H454.9C454.437 8.28224 452.419 10 450 10C447.581 10 445.563 8.28224 445.1 6H9.89998Z"
-            fill="black"
-          />
-          <mask
-            id="mask0_0_1"
-            style={{ maskType: "alpha" }}
-            maskUnits="userSpaceOnUse"
-            x="0"
-            y="0"
-            width="900"
-            height="10"
-          >
-            <path
-              d="M9.89998 6C9.43671 8.28224 7.41896 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.41896 0 9.43671 1.71776 9.89998 4H445.1C445.563 1.71776 447.581 0 450 0C452.419 0 454.437 1.71776 454.9 4H890.1C890.563 1.71776 892.581 0 895 0C897.761 0 900 2.23858 900 5C900 7.76142 897.761 10 895 10C892.581 10 890.563 8.28224 890.1 6H454.9C454.437 8.28224 452.419 10 450 10C447.581 10 445.563 8.28224 445.1 6H9.89998Z"
-              fill="#D9D9D9"
-            />
-          </mask>
-          <g mask="url(#mask0_0_1)">
-            <rect className="mask" y="-49" height="99" fill="black" />
-          </g>
-        </svg>
         {/* The section up act just as a wrapper. If the trigger (below) is the
       first jsx element in the component, you get an error on route change */}
 
@@ -207,10 +116,7 @@ const Project: React.FC = () => {
             className="h-screen w-[400vw] flex flex-row relative"
           >
             {data.map((item, index) => (
-              <section
-                key={index}
-                // className="h-screen w-[400vw] flex flex-row relative"
-              >
+              <section key={index}>
                 <div className="section h-screen w-screen justify-center items-center">
                   <span className="text-lg">{item.date}</span>
                   <h1 className="text-2xl">{item.title}</h1>
