df6754658327858134f5225b8d7f7438152df1b7
samsonc <samsonc0514@gmail.com>
2023-09-25 18:29:55 -0400

    update style of timeline data container

--- src/app/components/Project/Project.tsx
+++ src/app/components/Project/Project.tsx
@@ -1,142 +1,7 @@
-// "use client";
-// import React, { useEffect, useRef } from "react";
-// import Timeline from "../Timeline";
-// import data from "./TimelineData";
-// import gsap from "gsap";
-// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
-// const Project: React.FC = () => {
-//   console.log("ScrollSection component rendered"); // Add this line
-
-//   const timelineRef = useRef<HTMLDivElement>(null);
-
-//   const sectionRef = useRef<HTMLDivElement | null>(null);
-//   const triggerRef = useRef<HTMLDivElement | null>(null);
-
-//   gsap.registerPlugin(ScrollTrigger);
-//   console.log(sectionRef); // Add this line
-
-//   const bodyRef = useRef(null);
-//   const maskRef = useRef(null); // Needed for the max
-//   const projectBodyColour = "gray-200";
-
-//   const bodyClassName: string = `bg-${projectBodyColour} min-h-screen p-8`;
-//   // Unsure how to make the rectangle fill the whole screen, causes glitches in smaller screens
-//   const maskClassName: string = `absolute bg-${projectBodyColour} h-16 w-3/4`;
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       if (timelineRef.current) {
-//         const timelineBlocks = Array.from(timelineRef.current.children);
-//         timelineBlocks.forEach((block) => {
-//           if (
-//             block.getBoundingClientRect().top <= window.innerHeight * 0.75 &&
-//             block.classList.contains("is-hidden")
-//           ) {
-//             block.classList.remove("is-hidden");
-//             block.classList.add("bounce-in");
-//           }
-//         });
-//       }
-//     };
-
-//     gsap.to(maskRef.current, {
-//       width: "5%",
-//       x: 800,
-//       duration: 2,
-//       // Trying to figure out how to sync it with the scroll
-//       scrollTrigger: {
-//         trigger: bodyRef.current,
-//         markers: true,
-//         start: "top left",
-//         scrub: 1,
-//         pin: true,
-//       },
-//     });
-
-//     handleScroll();
-
-//     /*
-//     console.log("Running useEffect");
-
-//     if (!sectionRef.current || !triggerRef.current) {
-//       console.log("Refs not attached");
-//       return;
-//     }*/
-
-//     const pin = gsap.fromTo(
-//       sectionRef.current,
-//       {
-//         translateX: 0,
-//       },
-//       {
-//         translateX: "-300vw",
-//         ease: "none",
-//         duration: 1,
-//         scrollTrigger: {
-//           trigger: triggerRef.current,
-//           start: "top top",
-//           end: "2000 top",
-//           scrub: 0.6,
-//           pin: true,
-//           markers: true,
-//         },
-//       }
-//     );
-//     window.addEventListener("scroll", handleScroll);
-//   }, []);
-
-//   return (
-//     <div ref={bodyRef} className={bodyClassName}>
-//       <h2 className="text-3xl font-bold mb-8">Project Timeline</h2>
-//       <div>
-//         <div className="absolute">
-//           <Timeline
-//             className="flex flex-start h-16 opacity-100 "
-//             length={data.length}
-//           />
-//         </div>
-//         <div ref={maskRef} className={maskClassName}></div>
-//         <div className="absolute">
-//           <Timeline
-//             className="flex flex-start h-16 opacity-40"
-//             length={data.length}
-//           />
-//         </div>
-//       </div>
-//       <section className="">
-//         {/* The section up act just as a wrapper. If the trigger (below) is the
-//       first jsx element in the component, you get an error on route change */}
-
-//         {/* The div below act just as a trigger. As the doc suggests, the trigger and
-//       the animation should alway be two separated refs */}
-//         <div ref={triggerRef}>
-//           <div
-//             ref={sectionRef}
-//             className="h-screen w-[400vw] flex flex-row relative"
-//           >
-//             {data.map((item, index) => (
-//               <section key={index}>
-//                 <div className="section h-screen w-screen justify-center items-center">
-//                   <span className="text-lg">{item.date}</span>
-//                   <h1 className="text-2xl">{item.title}</h1>
-//                   <p className="text-base">{item.description}</p>
-//                 </div>
-//               </section>
-//             ))}
-//           </div>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default Project;
-
 "use client";
 import React, { useEffect, useRef } from "react";
 import Timeline from "../Timeline";
-import data from "./TimelineData";
+import timelineData from "./TimelineData";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
@@ -173,22 +38,22 @@ const Project: React.FC = () => {
           }
         });
       }
-    }; 
+    };
     let ctx = gsap.context(() => {
       gsap.to(maskRef.current, {
         width: "5%",
         translateX: `${
-          Math.ceil(7 * data.length) +
-          8 * data.length * (1920 / window.innerWidth - 1)
+          Math.ceil(7 * timelineData.length) +
+          8 * timelineData.length * (1920 / window.innerWidth - 1)
         }vw`,
         duration: 20,
         // Trying to figure out how to sync it with the scroll
-  
+
         scrollTrigger: {
           trigger: bodyRef.current,
           markers: true,
           start: "top top",
-          end: `+=${Math.ceil(1500 * Math.pow(data.length, 0.4))}`,
+          end: `+=${Math.ceil(1500 * Math.pow(timelineData.length, 0.4))}`,
           scrub: 1,
           pin: true,
         },
@@ -199,74 +64,67 @@ const Project: React.FC = () => {
           translateX: 0,
         },
         {
-          translateX: `${-250 - 7 * Math.pow(data.length, 1.35)}vw`,
+          translateX: `${-250 - 7 * Math.pow(timelineData.length, 1.35)}vw`,
           ease: "none",
           duration: 1,
           scrollTrigger: {
             trigger: triggerRef.current,
             start: "top top",
-            end: `${800 + 100 * Math.pow(data.length, 1.2)} top`,
+            end: `${800 + 100 * Math.pow(timelineData.length, 1.2)} top`,
             scrub: 0.6,
             pin: true,
             markers: true,
           },
         }
       );
-    })
-    
+    });
 
     handleScroll();
 
-    /*
-    console.log("Running useEffect");
-
-    if (!sectionRef.current || !triggerRef.current) {
-      console.log("Refs not attached");
-      return;
-    }*/
-
-    
     window.addEventListener("scroll", handleScroll);
 
     return () => ctx.revert();
   }, []);
 
   return (
-    <section ref={triggerRef} className=" overflow-hidden flex">
-      <div ref={bodyRef} className=" overflow-hidden ">
+    <section ref={triggerRef} className="overflow-hidden flex">
+      <div ref={bodyRef} className="overflow-hidden ">
         <div className={bodyClassName}>
           <h2 className="text-3xl font-bold mb-8">Project Timeline</h2>
-          <div className="">
+          <div className="flextems-center justify-center">
             <div className="absolute">
               <Timeline
                 className="flex flex-start h-16 opacity-100 "
-                length={data.length}
+                length={timelineData.length}
               />
             </div>
             <div ref={maskRef} className={maskClassName}></div>
             <div className="absolute">
               <Timeline
                 className="flex flex-start h-16 opacity-40"
-                length={data.length}
+                length={timelineData.length}
               />
             </div>
           </div>
 
           <div
             ref={sectionRef}
-            className="h-screen w-[400vw] flex flex-row relative"
+            className="h-screen w-[400vw] flex flex-row relative items-center justify-center"
           >
-            {data.map((item, index) => (
-              <section key={index} className="pin w-screen p-16">
-                <span className="anim">{item.date}</span>
-                <h1 className="anim text-7xl">{item.title}</h1>
-                <p className="anim text-lg">{item.description}</p>
+            {timelineData.map((item, index) => (
+              <section key={index} className="w-screen p-16 border text-center	">
+                <span className="item-center flex justify-center">
+                  {item.date}
+                </span>
+                <h1 className="text-5xl flex justify-center">{item.title}</h1>
+                <p className="text-lg italic leading-6 flex justify-center">
+                  {item.description}
+                </p>
               </section>
             ))}
           </div>
         </div>
       </div>
-      {/* </div> */}
     </section>
   );
 };
