7b5be7fcd7f6756415b2bfc26dc100e79db77ff5
samsonc <samsonc0514@gmail.com>
2023-09-29 00:03:08 -0400

    merging footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+
+import NewsLetter from "./NewsLetter";
+import SocialLink from "./SocialLink";
+import ScrollingBar from "./ScrollingBar";
+
+const iconsData = [
+  {
+    iconName: "BiLogoDiscordAlt",
+    socialLinkTitle: "Discord",
+    url: "https://www.google.com/",
+  },
+  {
+    iconName: "BiLogoInstagramAlt",
+    socialLinkTitle: "Instagram",
+    url: "https://www.google.com/",
+  },
+  {
+    iconName: "BiLogoLinkedinSquare",
+    socialLinkTitle: "LinkedIn",
+    url: "https://www.google.com/",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-950 text-white flex-col">
+      <div className="fixed inset-x-0 bottom-0 h-4/6">
+        <NewsLetter />
+        <div className="flex w-full border-solid border-white border-t-8 h-2/5 items-center justify-center">
+          {iconsData.map((data, index) => (
+            <SocialLink
+              key={index}
+              iconName={data.iconName}
+              socialLinkTitle={data.socialLinkTitle}
+              url={data.url}
+              hasRightBorder={index < iconsData.length - 1}
+            />
+          ))}
+        </div>
+        <ScrollingBar />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import Landing from "./components/Landing/landing";
 import Mission from "./components/Mission/mission";
 import Project from "./components/Project/project";
-import TeamCard from './components/TeamCard'
-
+import TeamCard from "./components/Team/TeamCard";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <main className="LBA bg-[#121A48]">
-        <Landing />
-        <Mission />
-        <Project />
-        <TeamCard />
+      <Landing />
+      <Mission />
+      <Project />
+      <TeamCard />
+      {/* <Footer /> */}
     </main>
   );
 }
