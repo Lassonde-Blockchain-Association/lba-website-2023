ae14b32793dd876e25d4bab93c94684a3182bdd6
samsonc <samsonc0514@gmail.com>
2023-09-29 18:43:35 -0400

    updated team card link and styling

--- src/app/components/Mission/missionParagraph.tsx
+++ src/app/components/Mission/missionParagraph.tsx
@@ -2,15 +2,13 @@ import React from "react";
 
 const MissionParagraph: React.FC = () => {
   return (
-    <main className="missionParagraphDiv">
-      <div className="flex ml-[40%] justify-center">
-        <p className="text-white text-left font-inter text-2xl font-normal font-weight-400 leading-normal w-[50%]">
-          We're a new studio designed to accelerate the advent of web3 gaming.
-          We develop and publish hybrid games that are mobile first and
-          blockchain-enabled.
-        </p>
-      </div>
-    </main>
+    <div className="flex ml-[40%] justify-center">
+      <p className="text-white text-left font-inter text-2xl font-normal font-weight-400 leading-normal w-[50%]">
+        We're a new studio designed to accelerate the advent of web3 gaming. We
+        develop and publish hybrid games that are mobile first and
+        blockchain-enabled.
+      </p>
+    </div>
   );
 };
 
