1abb35ff52b605b154d975baeae8a1f5531e9715
CHXiaoHua <63475258+CHXiaoHua@users.noreply.github.com>
2023-10-05 18:35:32 -0400

    ScrollingBar_Animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 
+import Footer from "./footer/page/Footer"
+
 export default function Home() {
   return (
     <main className="">
+      <Footer />
     </main>
   )
 }
